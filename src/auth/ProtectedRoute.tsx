4ae85bee6dcc8bb452609d2f9bdc583487aa2307
Miller <farwes1180@gmail.com>
2022-11-01 17:21:35 +0900

    add protected route

--- src/auth/ProtectedRoute.tsx
+++ src/auth/ProtectedRoute.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { ComponentType } from "react";
+import { Route } from "react-router-dom";
+import { withAuthenticationRequired } from "@auth0/auth0-react";
+import Loading from "src/components/common/loading/Loading";
 
-const ProtectedRoute = () => {
-  return <div>ProtectedRoute</div>;
+type ProtectedRouteProps = {
+  component: ComponentType;
+  [key: string]: any;
 };
+
+const ProtectedRoute = ({ component, ...args }: ProtectedRouteProps) => (
+  <Route
+    component={withAuthenticationRequired(component, {
+      onRedirecting: () => <Loading />,
+    })}
+    {...args}
+  />
+);
 
 export default ProtectedRoute;
