0ba92d0c5073711767681e633afaee2fe7bb02b5
Miller <farwes1180@gmail.com>
2022-11-01 18:20:14 +0900

    creating code edtior button

---
+++ src/components/common/header/CodeEditorButton.tsx
@@ -0,0 +1,19 @@
+import { Button, styled } from "@mui/material";
+import { useHistory } from "react-router-dom";
+import paths from "routes/paths";
+
+const CodeEditorStyledButton = styled(Button)(({ theme }) => ({
+  color: theme.commonColors.white,
+}));
+
+const CodeEditorButton = () => {
+  const history = useHistory();
+
+  const onClick = () => {
+    history.push(paths.codeEditor);
+  };
+
+  return <CodeEditorStyledButton onClick={onClick}>Sign In</CodeEditorStyledButton>;
+};
+
+export default CodeEditorButton;
