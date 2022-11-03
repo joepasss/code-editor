40b0a27e113b925f3e7c17f4d02fb86989c4e5a9
joepasss@linux <farwes1180@gmail.com>
2022-11-04 00:23:31 +0900

    create layout component

---
+++ src/layout/Layout.tsx
@@ -0,0 +1,22 @@
+import { styled } from "@mui/material";
+import Header from "components/common/header/Header";
+import { PropsWithChildren } from "react";
+
+const LayoutContainer = styled("div")({
+	height: "100%",
+	display: "flex",
+	flexDirection: "column",
+});
+
+const Page = styled("div")({
+	height: "100%",
+});
+
+const Layout = (props: PropsWithChildren<{}>) => {
+	return (
+		<LayoutContainer>
+			<Header />
+			<Page>{props.children}</Page>
+		</LayoutContainer>
+	);
+};
