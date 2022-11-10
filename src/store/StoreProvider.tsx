195046dca7d1261d71604547172d17f5ae77af51
Miller <farwes1180@gmail.com>
2022-11-10 10:41:48 +0900

    Adding persistence with Redux persist

--- src/store/StoreProvider.tsx
+++ src/store/StoreProvider.tsx
@@ -1,9 +1,17 @@
+import Loading from "components/common/loading/Loading";
 import { PropsWithChildren } from "react";
 import { Provider } from "react-redux";
-import store from "store";
+import { PersistGate } from "redux-persist/integration/react";
+import store, { persistor } from "store";
 
 const StoreProvider = (props: PropsWithChildren<{}>) => {
-	return <Provider store={store}>{props.children}</Provider>;
+  return (
+    <Provider store={store}>
+      <PersistGate loading={<Loading />} persistor={persistor}>
+        {props.children}
+      </PersistGate>
+    </Provider>
+  );
 };
 
 export default StoreProvider;
