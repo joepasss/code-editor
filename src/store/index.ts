db57c12978b4589bce02e1c320dad32e048da575
Miller <farwes1180@gmail.com>
2022-11-11 14:48:03 +0900

    Adding persistence with Redux persist

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,19 @@
+import { FLUSH, PAUSE, PERSIST, persistStore, PURGE, REGISTER, REHYDRATE } from "redux-persist";
+
 import { configureStore } from "@reduxjs/toolkit";
 import combinedReducers from "./root-reducer";
 
 const store = configureStore({
-	reducer: combinedReducers,
-	devTools: process.env.NODE_ENV !== "production",
+  reducer: combinedReducers,
+  devTools: process.env.NODE_ENV !== "production",
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
 
 export default store;
--- src/store/root-reducer.ts
+++ src/store/root-reducer.ts
@@ -1,8 +1,17 @@
+import storage from "redux-persist/lib/storage";
 import { combineReducers } from "@reduxjs/toolkit";
 import { darkModeReducer } from "./slices/dark-mode/darkMode";
+import { persistReducer } from "redux-persist";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
 
 const combinedReducers = combineReducers({
-	darkMode: darkModeReducer,
+  darkMode: darkModeReducer,
 });
 
-export default combinedReducers;
+const persistedReducer = persistReducer(persistConfig, combinedReducers);
+
+export default persistedReducer;
